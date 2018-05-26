d2f03122bbf019dc9c295fbe92ac73847b82ffd4
fgjhhkfk <fgjhhkfk@googlemail.com>
2018-05-26 12:18:20 +0200

    immer weiter

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rezepte.sqlite'
+db = SQLAlchemy(app)
+
+class Rezepte(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titel = db.Column(db.String(64))
+    link = db.Column(db.String(64))
+    bild = db.Column(db.String(64))
+    thumbnail = db.Column(db.String(64))
+    zutaten = db.Column(db.String(64))
+    zubereitung = db.Column(db.String(64))
+
+# rezepte = [
+#         {'link':'./rezepte/Haehnchen_orientalisch.html',\
+#         'bild':'./bilder/thumbnails/Haehnchen_orientalisch.jpg',\
+#         'titel':'Heahnchen orientalisch'},\
+#         {'link':'./rezepte/Haehnchenbrust_a_la_saltimboca.html',\
+#         'bild':'./bilder/Haehnchenbrust_a_la_Saltimboca.png',\
+#         'titel':'Haehnchenbrust a la Saltimboca'}
+#         ]
 
 @app.route('/')
 def home():
@@ -16,7 +38,8 @@ def rezept():
 
 @app.route('/main.html')
 def main():
-    return render_template('main.html')
+    rezepte = Rezepte.query.all()
+    return render_template('main.html', rezepte=rezepte)
 
 @app.route('/dolce.html')
 def dolce():
