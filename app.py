020bf14d53549598698bdea80cd385d7875e48af
fgjhhkfk <fgjhhkfk@googlemail.com>
2018-05-26 20:19:52 +0200

    Added searchbar with whoosh.

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+import flask_whooshalchemy as wa
+import sqlite3
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rezepte.sqlite'
+app.config['WHOOSH_BASE'] = 'whoosh'
+
+db = SQLAlchemy(app)
+
+class Rezepte(db.Model):
+    __searchable__ = ['titel', 'zutaten', 'zubereitung']
+    id = db.Column(db.Integer, primary_key=True)
+    titel = db.Column(db.String(64))
+    link = db.Column(db.String(64))
+    bild = db.Column(db.String(64))
+    thumbnail = db.Column(db.String(64))
+    zutaten = db.Column(db.String(64))
+    zubereitung = db.Column(db.String(64))
+
+wa.whoosh_index(app, Rezepte)
 
 @app.route('/')
 def home():
@@ -14,9 +33,15 @@ def about():
 def rezept():
     return render_template('rezept.html')
 
-@app.route('/main.html')
+@app.route('/main.html', methods=['GET','POST'])
 def main():
-    return render_template('main.html')
+    rezepte = Rezepte.query.all()
+    return render_template('main.html', rezepte=rezepte)
+
+@app.route('/search')
+def search():
+    rezepte = Rezepte.query.whoosh_search(request.args.get('query')).all()
+    return render_template('main.html', rezepte=rezepte)
 
 @app.route('/dolce.html')
 def dolce():
