830b4b2901c4a6b1234c8afe0f1da0890595b38d
fgjhhkfk <fgjhhkfk@googlemail.com>
2018-06-06 22:40:24 +0200

    Einzelne Rezepte auswaehlen geht jetzt

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-from flask import Flask, render_template, request
-# from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
-import flask_whooshalchemy as wa
-# import sqlite3
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///foobar.sqlite'
-app.config['WHOOSH_BASE'] = 'whoosh'
 
 db = SQLAlchemy(app)
 
@@ -24,39 +21,36 @@ class Rezepte(db.Model):
     tags = db.Column(db.String(64))
 
 
-wa.whoosh_index(app, Rezepte)
-
-
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
-@app.route('/rezept.html')
-def rezept():
-    rezepte = Rezepte.query.all()
-    return render_template('rezept.html', rezepte=rezepte)
+@app.route('/rezept/<int:id>/')
+def rezept(id):
+    rezept = Rezepte.query.get_or_404(id)
+    return render_template('rezept.html', rezept=rezept)
 
 
-@app.route('/main.html', methods=['GET', 'POST'])
+@app.route('/main', methods=['GET', 'POST'])
 def main():
     rezepte = Rezepte.query.filter(Rezepte.kategorie == 'Main')
     return render_template('main.html', rezepte=rezepte)
 
 
-@app.route('/dolce.html')
+@app.route('/dolce')
 def dolce():
     rezepte = Rezepte.query.filter(Rezepte.kategorie == 'Dolce')
     return render_template('dolce.html', rezepte=rezepte)
 
 
-@app.route('/saucen.html')
+@app.route('/saucen')
 def saucen():
     rezepte = Rezepte.query.filter(Rezepte.kategorie == 'Saucen')
     return render_template('saucen.html', rezepte=rezepte)
 
 
-@app.route('/beilagen.html')
+@app.route('/beilagen')
 def beilagen():
     rezepte = Rezepte.query.filter(Rezepte.kategorie == 'Beilagen')
     return render_template('beilagen.html', rezepte=rezepte)
@@ -64,7 +58,8 @@ def beilagen():
 
 @app.route('/search')
 def search():
-    rezepte = Rezepte.query.filter(Rezepte.zutaten.like("%" + request.args.get('query') + "%"))
+    rezepte = Rezepte.query.filter(Rezepte.zutaten.like
+                                   ("%" + request.args.get('query') + "%"))
     return render_template('main.html', rezepte=rezepte)
 
 
