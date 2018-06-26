d7498216c4e94be3d1caa56ab1b95fa1331ffbf9
fgjhhkfk <fgjhhkfk@googlemail.com>
2018-06-26 23:37:11 +0200

    Darstellung der neues Rezept Seite rumgebastelt.

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import DataRequired
 
 
@@ -8,7 +8,8 @@ class RezeptForm(FlaskForm):
     titel = StringField('Titel', validators=[DataRequired()])
     zutaten = TextAreaField('Zutaten', validators=[DataRequired()])
     zubereitung = TextAreaField('Zubereitung', validators=[DataRequired()])
-    kategorie = StringField('Kategorie', validators=[DataRequired()])
-    tags = StringField('Tags', validators=[DataRequired()])
+    # kategorie = StringField('Kategorie', validators=[DataRequired()])
+    kategorie = SelectField(u'Kategorie', choices=[('Main', 'Main'), ('Dolce', 'Dolce'), ('Saucen', 'Saucen'), ('Beilagen', 'Beilagen')])
+    tags = StringField('Tags')
     bild = FileField('Bild', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Hochladen')
